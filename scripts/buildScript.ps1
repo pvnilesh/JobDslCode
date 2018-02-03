8083b60921bcf42cd32a2f03e863b6584a819fea
nilesh <nileshpadm@gmail.com>
2018-02-03 20:27:31 +0530

    commit03

---
+++ scripts/buildScript.ps1
@@ -0,0 +1,19 @@
+$metaFile = "metadata.txt"
+$jsonFile = ConvertFrom-Json "$(Get-Content $metaFile)"
+$artifactName = $jsonFile.Name
+$artifactVersion = $jsonFile.Version
+
+$targetPath = "C:\Jenkins\target"
+If(!(test-path $targetPath))
+{
+      New-Item -ItemType Directory -Path $targetPath
+}
+
+
+$srcFile = "${artifactName}.txt"
+$OutFile = "${artifactName}-${$artifactVersion}.zip"
+$targetFile = "$(Join-Path $targetPath $outFile)"
+if (!(Test-Path $targetFile))
+{
+   Compress-Archive -Path $srcFile -DestinationPath $targetFile
+}
