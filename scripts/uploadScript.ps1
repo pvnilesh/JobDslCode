ae818f926f5ef35d1ecfba266ffce5487ade4cb4
nilesh <nileshpadm@gmail.com>
2018-02-04 10:05:25 +0530

    commit07

--- scripts/uploadScript.ps1
+++ scripts/uploadScript.ps1
@@ -1,5 +1,6 @@
-Write-Output $NEXUS_SERVER
-Write-Output $NEXUS_PASSWORD
-Write-Output $NEXUS_SERVER
-Write-Output $NEXUS_REPOSITORY
-Write-Output $SOURCE_FILE_PATH
+$arr = $SOURCE_FILE_PATH.split('\')
+$fileName = $arr[-1]
+$urlDest = "{0}/repository/{1}/{2}" -f ($NEXUS_SERVER, $NEXUS_REPOSITORY, $fileName)
+$webClient = New-Object System.Net.WebClient
+$webClient.Credentials = New-Object System.Net.NetworkCredential($NEXUS_USERNAME, $NEXUS_PASSWORD)
+$webClient.UploadFile($urlDest, "PUT", $SOURCE_FILE_PATH)
