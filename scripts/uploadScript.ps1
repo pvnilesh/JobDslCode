57eb2073527f5ae248d244b9c99ed09e4d2681c3
nilesh <nileshpadm@gmail.com>
2018-02-04 10:11:09 +0530

    commit08

--- scripts/uploadScript.ps1
+++ scripts/uploadScript.ps1
@@ -1,6 +1,6 @@
-$arr = $SOURCE_FILE_PATH.split('\')
+$arr = $env:SOURCE_FILE_PATH.split('\')
 $fileName = $arr[-1]
-$urlDest = "{0}/repository/{1}/{2}" -f ($NEXUS_SERVER, $NEXUS_REPOSITORY, $fileName)
+$urlDest = "{0}/repository/{1}/{2}" -f ($env:NEXUS_SERVER, $env:NEXUS_REPOSITORY, $fileName)
 $webClient = New-Object System.Net.WebClient
-$webClient.Credentials = New-Object System.Net.NetworkCredential($NEXUS_USERNAME, $NEXUS_PASSWORD)
-$webClient.UploadFile($urlDest, "PUT", $SOURCE_FILE_PATH)
+$webClient.Credentials = New-Object System.Net.NetworkCredential($env:NEXUS_USERNAME, $env:NEXUS_PASSWORD)
+$webClient.UploadFile($urlDest, "PUT", $env:SOURCE_FILE_PATH)
