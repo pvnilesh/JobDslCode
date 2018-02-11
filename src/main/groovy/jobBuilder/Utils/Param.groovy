c39a2e1dd0b59bd3739993458e1f7dff04842916
nilesh <nileshpadm@gmail.com>
2018-02-11 13:31:49 +0530

    commit50

--- src/main/groovy/jobBuilder/Utils/Param.groovy
+++ src/main/groovy/jobBuilder/Utils/Param.groovy
@@ -4,15 +4,11 @@ import java.util.ArrayList
 
 public class Param {
 	static void paramConfig(context, ArrayList<String[]> list){
-		def myParam = new String[3]
-
-		myParam[0] = "RepositoryName"
-		myParam[1] = ""
-		myParam[2] = ""
-		context.with {
-		    stringParam(myParam.getClass().toString(),myParam[0].getClass().toString())
+		for(item in list){
+		   context.with {
+		     stringParam(item[0],item[1],item[2])
+		   }  
 		}
-
 	}
 	
     static String requiredString(String[] mypara) {
