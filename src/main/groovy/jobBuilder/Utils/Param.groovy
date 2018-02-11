70599d4a801757a39845e7ae42257d344ac6d2ee
nilesh <nileshpadm@gmail.com>
2018-02-11 15:37:53 +0530

    commit63

--- src/main/groovy/jobBuilder/Utils/Param.groovy
+++ src/main/groovy/jobBuilder/Utils/Param.groovy
@@ -4,15 +4,10 @@ import java.util.ArrayList
 
 public class Param {
 	static void paramConfig(context, ArrayList<String[]> list){
-		def abc = "a"
-		def qwe = "b"
-		def asd = "c"
-		def i = 1
 		for(item in list){
 		   	context.with {
-				StringParam(abc+i,qwe+i,asd+i)
+				stringParam((item[0],item[1],item[2])
 			}
-			i++
 		} 
 	}
 }
