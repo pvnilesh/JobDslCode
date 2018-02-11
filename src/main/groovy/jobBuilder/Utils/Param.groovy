1d769590175cf57924d1d84e02bc7caccf2d88d4
nilesh <nileshpadm@gmail.com>
2018-02-11 15:01:18 +0530

    commit60

--- src/main/groovy/jobBuilder/Utils/Param.groovy
+++ src/main/groovy/jobBuilder/Utils/Param.groovy
@@ -6,7 +6,7 @@ public class Param {
 	static void paramConfig(context, ArrayList<String[]> list){
 		for(item in list){
 		   	context.with {
-				StringParam(${item[0]}.toString(),${item[1]}.toString(),${item[2]}.toString())
+				StringParam("${item[0]}".toString(),"${item[1]}".toString(),"${item[2]}".toString())
 			}
 		} 
 	}
