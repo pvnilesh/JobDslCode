173de79e747638a9830ce1444bb4aa6fb27e2cec
nilesh <nileshpadm@gmail.com>
2018-02-13 21:53:32 +0530

    commit68

--- src/jobs/test.groovy
+++ src/jobs/test.groovy
@@ -9,55 +9,51 @@ Class Steps = new GroovyClassLoader(getClass().getClassLoader()).parseClass(sour
 String sourceFile3 = readFileFromWorkspace("src/main/groovy/jobBuilder/Utils/Param.groovy")
 Class Param = new GroovyClassLoader(getClass().getClassLoader()).parseClass(sourceFile3)
 
-def myParamList = new ArrayList<String[]>()
-def myStepList = new ArrayList<String>()
+def myStepList = new ArrayList<String[]>()
 
-if(ScmType.equalsIgnoreCase("GIT")) { 
-	def myParam1 = new String[3]
-	def myParam2 = new String[3]
-
-	myParam1[0] = "RepositoryName"
-	myParam1[1] = ""
-	myParam1[2] = ""
-	myParamList.add(myParam1)
-
-	myParam2[0] = "BranchName"
-	myParam2[1] = "master"
-	myParam2[2] = ""
-	myParamList.add(myParam2)
+if(DoCompile.equalsIgnoreCase("TRUE")){
+	def myParam1 = new String[2]
+	
+	myParam1[0] = "CompileScript"
+	myParam1[1] = "CompileScriptParamFile"
+	myStepList.add(myParam1)
 }
 
-if(doCompile.equalsIgnoreCase("TRUE")){
-	def myParam3 = new String[3]
+if(DoPackage.equalsIgnoreCase("TRUE")){
+	def myParam2 = new String[2]
 	
-	myParam3[0] = "CompileScript"
-	myParam3[1] = "compile.sh"
-	myParam3[2] = ""
-	myParamList.add(myParam3)
-	
-	myStepList.add('CompileScript')
+	myParam2[0] = "PackageScript"
+	myParam2[1] = "PackageScriptParamFile"
+	myStepList.add(myParam2)
 }
 
-if(doPackage.equalsIgnoreCase("TRUE")){
-	def myParam4 = new String[3]
-
-	myParam4[0] = "PackageScript"
-	myParam4[1] = "package.sh"
-	myParam4[2] = ""
-	myParamList.add(myParam4)
-	
-	myStepList.add('PackageScript')
-}
-
-job("Test/built-with-utils") {
+job("${FolderName}/${AppName}") {
     logRotator(2, 10, -1, -1)
     scm {
         Scm.myscm(delegate,ScmType)
     }
     parameters {
-        Param.paramConfig(delegate,myParamList)
+        stringParam('NexusServer','35.201.168.32:8081','')
+		stringParam('NexusRepository','LaserRaw','')
+		stringParam('NexusUsername','admin','')
+		stringParam('NexusPassword','admin123','')
     }
     steps {
 		Steps.myShell(delegate,myStepList)
     }
+	wrappers {
+    envInjectBuildWrapper {
+      info {
+        propertiesFilePath(PropertiesFile)
+        propertiesContent("")
+        scriptFilePath("")
+        scriptContent("")
+        loadFilesFromMaster(false)
+        secureGroovyScript {
+          script("")
+          sandbox(false)
+        }  
+      }  
+    }  
+}	
 }
--- src/main/groovy/jobBuilder/Utils/Steps.groovy
+++ src/main/groovy/jobBuilder/Utils/Steps.groovy
@@ -1,10 +1,10 @@
 package jobBuilder.Utils
 
 public class Steps {
-	static void myShell(context, ArrayList<String> list) {
+	static void myShell(context, ArrayList<String[]> list) {
 		for(item in list){
 		   	context.with {
-				shell("./\$$item")
+				shell('./$' + item[0] + ' `cat $' + item[1] + '`')
 			}
 		}
     }
