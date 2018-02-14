696cec0c3f1a449e52bdd8715d590a70e4435189
pvnilesh <nileshpadm@gmail.com>
2018-02-14 10:29:27 +0530

    changed scm.groovy file

--- src/main/groovy/jobBuilder/Utils/Scm.groovy
+++ src/main/groovy/jobBuilder/Utils/Scm.groovy
@@ -1,13 +1,13 @@
 package jobBuilder.Utils
 
 public class Scm {
-    static void myscm(context, String scmType) {
+    static void myscm(context, String scmType,ArrayList<String> list) {
 		if(scmType.equalsIgnoreCase("GIT")){
 		    context.with {
 				git {
 					remote {
-						url '$RepositoryName'
-						branch '*/$BranchName'
+						url list[0]
+						branch list[1]
 					}
 				}
 			}
